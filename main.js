cc779f5fc7ffc94266df8b3435d42ac3b19776a5
Alexander_Fields <Zyvod1300@yahoo.com>
2023-10-25 13:43:04 -0500

    Added css file, tested AJAX call, added header searchbar and search button

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+const body = document.getElementsByTagName('body')
 
+
+console.log($.get('https://store.steampowered.com/api/storesearch/?term=halo&=english&cc=NL', (data) => {
+ console.log(data)
+}))
+
+
+console.log($.get('https://store.steampowered.com/api/getappsingenre/?genre=rpg&cc=us&l=english', (data) => {
+ console.log(data)
+}))
+
+
+
+//What data do we get from each search?
+
+// each item contains the following keys [ id,metascore,name,platforms,price,stereamingvideo,tiny_image,type]
+
+// the key platforms contains the keys [linux,mac,windows]
+// the key price contains the keys [ currency,final,initial]
