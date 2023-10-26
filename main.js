91c6a0b31395cbecf6b012de6ecdf0e46867a0f6
Alexander_Fields <Zyvod1300@yahoo.com>
2023-10-25 19:25:32 -0500

    Created a link to each game, cleared search results before new search, debugged exclusion of results

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//What data do we get from each search?
+
+// each item contains the following keys [ id,metascore,name,platforms,price,stereamingvideo,tiny_image,type]
+
+// the key platforms contains the keys [linux,mac,windows]
+// the key price contains the keys [ currency,final,initial]
+//Discovered that we can append the id from search results to a partial link from steam in order to create a link to the game on our application
+
 const body = $('body')
 const searchBar = $('#searchBar')
 const searchBtn = $('#searchBtn')
@@ -20,62 +28,54 @@ searchBtn.on('click', (event) => {
 
 function populateResults(gameData) {
   const searchResults = document.getElementById('searchResults');
-
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
   while (searchResults.firstChild) {
     searchResults.removeChild(searchResults.firstChild);
   }
-
-    for (let i = 0; i < gameData.length; i++) {
+      gameData.forEach(function(game){
       const gameResult = document.createElement('span');
       gameResult.classList.add('game-result');
 
       const gameTitle = document.createElement('h3');
       gameTitle.classList.add('game-title');
-      gameTitle.textContent = gameData[i].name;
+      gameTitle.textContent = game.name;
       gameResult.appendChild(gameTitle);
 
       const gameImage = document.createElement('img');
       gameImage.classList.add('game-image');
-      gameImage.src = gameData[i].tiny_image;
+      gameImage.src = game.tiny_image;
       gameResult.appendChild(gameImage);
 
-      // const gameType = document.createElement('h2');
-      // gameType.classList.add('game-type');
-      // gameType.textContent = gameData[i].type;
-      // gameResult.appendChild(gameType);
+      const gameLink = document.createElement('a');
+      gameLink.classList.add('game-link');
+      gameLink.textContent = 'Link'
+      gameLink.setAttribute('href', `https://store.steampowered.com/app/${game.id}`) ;
+      gameResult.appendChild(gameLink);
 
       const gameDetails = document.createElement('div');
       gameDetails.classList.add('game-details');
 
       const metascore = document.createElement('p');
-      metascore.innerHTML = `<b>METASCORE:</b> ${gameData[i].metascore}`;
+      metascore.innerHTML = `<b>METASCORE:</b> ${game.metascore}`;
       gameDetails.appendChild(metascore);
 
       const operatingSystems = document.createElement('p');
-      operatingSystems.innerHTML = `<b>OPERATING SYSTEMS:</b> ${gameData[i].platforms}`;
+      operatingSystems.innerHTML = `<b>OPERATING SYSTEMS:</b> ${game.platforms}`;
       gameDetails.appendChild(operatingSystems);
 
-      const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      });
-
-      if ( gameData[i].price === undefined ) {
-       continue
-      } else {
       const price = document.createElement('p');
-      price.innerHTML = `<b>PRICE:</b> ${formatter.format(gameData[i].price.final/100)}`;
+       if ( 'price' in game ) {
+        price.innerHTML = `<b>PRICE:</b> ${formatter.format(game.price.final/100)}`;
+        } else {
+        price.innerHTML = `<b>PRICE:</b> No Price Available`;
+        }
       gameDetails.appendChild(price);
-      }
 
       gameResult.appendChild(gameDetails);
       searchResults.appendChild(gameResult);
-    }
+    })
  }
 
-//What data do we get from each search?
-
-// each item contains the following keys [ id,metascore,name,platforms,price,stereamingvideo,tiny_image,type]
-
-// the key platforms contains the keys [linux,mac,windows]
-// the key price contains the keys [ currency,final,initial]
